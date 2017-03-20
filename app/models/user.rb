f641a553b6b2d25dbee7afe9ea4416ca8ff4feeb
Evianne Keeney <evianneelise@gmail.com>
2017-03-20 11:15:44 -0400

    Adds codeship badge

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_many :plants
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true
+  validates :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
