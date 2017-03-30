9e1472cc0ec673edb1258c83ca603938290a6a99
Evianne Keeney <evianneelise@gmail.com>
2017-03-29 21:28:41 -0400

    adding email credentials

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -3,9 +3,14 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  class ProfilePhotoUploader < CarrierWave::Uploader::Base
+    if Rails.env.test?
+      storage :file
+    else
+      storage :fog
+    end
+  end
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
