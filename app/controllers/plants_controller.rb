c2b7c5b7bd223fb9723b7a499eee5526edb316c5
Evianne Keeney <evianneelise@gmail.com>
2017-04-02 03:38:31 -0400

    weather styling

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,12 +3,13 @@ class PlantsController < ApplicationController
 
   def index
     @plants = Plant.all
+    @temperature = Temperature.new(current_user.latitude, current_user.longitude)
+
   end
 
   def show
     @plants = Plant.find(params[:id])
     @temperature = Temperature.new(current_user.latitude, current_user.longitude)
-
   end
 
   def new
