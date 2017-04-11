ad7333e1fb4cfef210b4895e8c51c82cfe8cde33
Evianne Keeney <evianneelise@gmail.com>
2017-04-10 20:36:01 -0400

    plant button not styled properly, this moves some css around

--- react/src/components/PlantItem.js
+++ react/src/components/PlantItem.js
@@ -15,7 +15,7 @@ const PlantItem = (props) => {
         </div>
         <div className="back">
           <button className={props.className} onClick={props.handleClick(props.id)}>
-          <div className="showlink" href={`https://plant-a-garden.herokuapp.com/plants/` + props.id}> {props.name} </div>
+          <div className="showlink" href={`http://localhost:3000/plants/` + props.id}> {props.name} </div>
               <p>Days you have left: </p>
               <p>{props.time_left}</p>
           </button>
